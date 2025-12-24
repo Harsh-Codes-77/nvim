fe24759e3d07a8da202f1ebfd2d034681a1b024a
harsh <pharshpathak703@gmail.com>
2025-12-24 23:16:41 +0530

    Setup Complete

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -62,21 +62,19 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    lazy = false, -- Load immediately to avoid race conditions
-    config = function()
-      local configs = require("nvim-treesitter.configs")
-      configs.setup({
-        ensure_installed = { "lua", "vim", "vimdoc", "query", "javascript", "html", "css", "python", "markdown", "markdown_inline" },
-        auto_install = true,
-        highlight = {
-          enable = true,
-          additional_vim_regex_highlighting = false,
-        },
-        indent = {
-          enable = true,
-        },
-      })
-    end,
+    lazy = false,
+    main = "nvim-treesitter.configs",
+    opts = {
+      ensure_installed = { "lua", "vim", "vimdoc", "query", "javascript", "html", "css", "python", "markdown", "markdown_inline" },
+      auto_install = true,
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+      },
+      indent = {
+        enable = true,
+      },
+    },
   },
   
   -- Gitsigns
